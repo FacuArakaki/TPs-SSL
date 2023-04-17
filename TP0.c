65255a60656566627e7eea858a0531ca193a6f87
FacuArakaki <farakaki@frba.utn.edu.ar>
2023-04-17 10:24:47 -0300

    Le cambie para que genere un txt

--- TP0.c
+++ TP0.c
@@ -1,7 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<string.h>
 
-int main(){
-    printf("Hola Mundo");
+int main() {
+    char Holatodos[]=" Hola a todos";
+    
+    FILE* archivo = fopen("EjerEnClase.txt","wb+");
+            fseek(archivo, 0, SEEK_END);
+            fprintf(archivo, "%s", Holatodos);
+            //fputs("\n",archivo);
+        
+        fclose(archivo);
 
-    return 0;
+    printf("VAmos los pibes");
+
+   return 0;
 }
